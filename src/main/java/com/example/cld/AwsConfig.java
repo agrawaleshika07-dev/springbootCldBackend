69df107b2e9e23fe584af131420d96a3d9907667
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-06 15:33:13 +0530

    Backend code commit

--- src/main/java/com/example/cld/AwsConfig.java
+++ src/main/java/com/example/cld/AwsConfig.java
@@ -1,13 +1,16 @@
 package com.example.cld;
 
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.ec2.Ec2Client;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.sqs.SqsClient;
+
 
 @Configuration
 public class AwsConfig {
@@ -16,26 +19,26 @@ public class AwsConfig {
     private String awsRegion;
 
     @Bean
-    public S3Client s3Client() {
-        return S3Client.builder()
-                .region(Region.of(awsRegion))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(awsRegion)
+                .withCredentials(DefaultCredentialsProvider.create())
                 .build();
     }
 
     @Bean
-    public Ec2Client ec2Client() {
-        return Ec2Client.builder()
-                .region(Region.of(awsRegion))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+    public AmazonEC2 amazonEC2() {
+        return AmazonEC2ClientBuilder.standard()
+                .withRegion(awsRegion)
+                .withCredentials(DefaultCredentialsProvider.create())
                 .build();
     }
 
     @Bean
-    public SqsClient sqsClient() {
-        return SqsClient.builder()
-                .region(Region.of(awsRegion))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+    public AmazonSQS amazonSQS() {
+        return AmazonSQSClientBuilder.standard()
+                .withRegion(awsRegion)
+                .withCredentials(DefaultCredentialsProvider.create())
                 .build();
     }
 }
