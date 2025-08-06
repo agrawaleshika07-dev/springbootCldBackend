fee33f8388f78d61e3126df5a9986464319c2b68
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-06 16:16:07 +0530

    Backend code commit

--- src/main/java/com/example/cld/MessageService.java
+++ src/main/java/com/example/cld/MessageService.java
@@ -19,6 +19,8 @@ public class MessageService {
     }
 
     public List<String> getAllMessages() {
-        return messageRepository.findAll().stream().map(Message::getContent).collect(Collectors.toList());
+        return messageRepository.findAll().stream()
+                .map(Message::getContent)
+                .collect(Collectors.toList());
     }
 }
