754d36455a75c433e256b276da2063e8b5c4555f
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-06 15:30:09 +0530

    Backend code commit

---
+++ src/main/java/com/example/cld/Message.java
@@ -0,0 +1,31 @@
+package com.example.cld;
+
+import javax.persistence.*;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    public Message() {}
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
