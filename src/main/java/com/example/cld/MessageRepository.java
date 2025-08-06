24172ebef1d4a4f53247f2551d2f0118225eb1e7
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-06 15:58:46 +0530

    Backend code commit

--- src/main/java/com/example/cld/MessageRepository.java
+++ src/main/java/com/example/cld/MessageRepository.java
@@ -2,5 +2,8 @@ package com.example.cld;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.persistence.Entity;
+
 public interface MessageRepository extends JpaRepository<Message, Long> {
 }
+
